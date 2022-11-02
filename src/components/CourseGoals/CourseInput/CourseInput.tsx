28ad24ca4e88881c99e59afd5367f08fb5d34f2b
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-02 10:24:35 +0200

    add invalid state

--- src/components/CourseGoals/CourseInput/CourseInput.tsx
+++ src/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -1,25 +1,40 @@
-import { FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 interface CourseInputProps {
-    onAddGoal: (val: string) => void
+  onAddGoal: (val: string) => void
 }
 
-function CourseInput({onAddGoal}:CourseInputProps) {
+function CourseInput({ onAddGoal }: CourseInputProps) {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   function formSubmitHandler(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    if (enteredValue.trim() === '') {
+      setIsValid(false)
+      return
+    }
     onAddGoal(enteredValue);
   };
 
+  function valueChangeHandler(e: ChangeEvent<HTMLInputElement>) {
+    setEnteredValue(e.target.value);
+    if (e.target.value.trim() !== '') {
+      setIsValid(true)
+    }
+  }
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control${!isValid ? ' invalid' : ''}`}>
         <label>Course Goal</label>
-        <input type="text" onChange={e=>setEnteredValue(e.target.value)} value={enteredValue} />
+        <input
+          type="text"
+          onChange={valueChangeHandler}
+          value={enteredValue} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
